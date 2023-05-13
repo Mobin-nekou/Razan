05d27e7aa7e7137c4f4d3de3ff2a6532fcc62ca2
users1 <jab.neko12@gmail.com>
2023-05-13 21:51:15 +0430

    final one

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 
 
-document.querySelector('.button').addEventListener('click' , () => {
+document.querySelector('.button').addEventListener('click' , (e) => {
+
+    e.preventDefault();
     
     document.querySelector('.message').classList.toggle('worning')
 
 })
+
